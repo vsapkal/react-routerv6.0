647788a9471b779e4211e59e0ef6f1775a30f573
Vinod_sapkal78 <vinod.sapkal@infostretch.com>
2022-05-30 18:23:56 +0530

    Toggle condition in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [toggle, setToggle] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      {toggle ? <h3>Welcome to the World of React</h3> : null}
     </div>
   );
 }
