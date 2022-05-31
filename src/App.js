c005350dff4cf99350eff9b00d598060d24a51dd
Vinod_sapkal78 <vinod.sapkal@infostretch.com>
2022-05-31 10:42:13 +0530

    VideoNo.01  React router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Component/Home';
+import Navbar from './Component/Navbar';
+import About from './Component/About';
+import Contact from './Component/Contact';
 
 function App() {
-  const [toggle, setToggle] = useState(true);
   return (
-    <div className="App">
-      <button onClick={() => setToggle(!toggle)}>Toggle</button>
-      {toggle ? <h3>Welcome to the World of React</h3> : null}
-    </div>
+    <>
+      <div className="App">
+        <h3>Welcome to the World of React Router V6.0</h3>
+      </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' />
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
